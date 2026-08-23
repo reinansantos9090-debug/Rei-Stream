18f36f953f76e16aa206d9373cd497ffdab5b65a
reinansantos9090-debug <reinansantos9090@gmail.com>
2026-08-22 22:16:49 -0300

    RepositoryManager.kt

--- app/src/main/java/com/lagradost/cloudstream3/plugins/RepositoryManager.kt
+++ app/src/main/java/com/lagradost/cloudstream3/plugins/RepositoryManager.kt
@@ -98,8 +98,18 @@ data class PluginWrapper(
 object RepositoryManager {
     const val ONLINE_PLUGINS_FOLDER = "Extensions"
     val PREBUILT_REPOSITORIES: Array<RepositoryData> by lazy {
-        getKey<Array<RepositoryData>>("PREBUILT_REPOSITORIES") ?: emptyArray()
-    }
+    arrayOf(
+        RepositoryData(
+            "Rei-Stream Repositório PT-BR",
+            "https://raw.githubusercontent.com/recloudstream/cloudstream-extensions-br/builds/repo.json"
+        ),
+        RepositoryData(
+            "English Providers",
+            "https://raw.githubusercontent.com/recloudstream/cloudstream-extensions/builds/repo.json"
+        )
+    )
+}
+
     private val GH_REGEX =
         Regex("^https://raw.githubusercontent.com/([A-Za-z0-9-]+)/([A-Za-z0-9_.-]+)/(.*)$")
 
